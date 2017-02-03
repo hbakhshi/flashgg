b04cd8607e80f05598c9ad5738df21a6ff07499e
Hamed <hbakhshi@cern.ch>
2017-02-03 16:51:02 +0100

    more backgrounds

--- Systematics/test/data_jobs_forthq.sh
+++ Systematics/test/data_jobs_forthq.sh
@@ -5,5 +5,5 @@ queue="1nd"
 #LM=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt
 LM=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_23Sep2016ReReco_Collisions16_JSON.txt
 outdir="/afs/cern.ch/user/h/hbakhshi/work/tHq/CMSSW_8_0_25/src/flashgg/Systematics/test/"
-version="1"
+version="2"
 fggRunJobs.py --load data_jobs_forthq.json -d $outdir/data_jobs_${version} -x cmsRun workspaceStd_testThq.py maxEvents=-1 -n 500 -q ${queue} -D -P useAAA=1 --no-use-tarball lumiMask=${LM}
